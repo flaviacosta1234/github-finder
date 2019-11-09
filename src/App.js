9b079642b9ec245ae1667dc1f130319b1ca569af
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 16:39:47 +0000

    Passar proptype inválid

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
+    const numbers = [1, 2, 3, 4];
+
     return (
       <div className="App">
-        <Navbar />
+        <Navbar title={numbers} />
       </div>
     );
   }
