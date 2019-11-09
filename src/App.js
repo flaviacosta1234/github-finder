ee4dc0e7c4045f88f0a3dbd60574fdca3d56469e
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 15:47:26 +0000

    Loading...

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import "./App.css";
 class App extends Component {
   render() {
     const name = "John Doe";
+    const loading = true;
+
+    if (loading) {
+      return <h4>Loading...</h4>;
+    }
+
     return (
       <div className="App">
-        <h1>Hello {name.toUpperCase()}</h1>
+        <h1>Hello {name}</h1>
       </div>
     );
   }
