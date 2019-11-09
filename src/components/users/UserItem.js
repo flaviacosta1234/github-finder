e9e59ca368a14472ff36695a3a2818e755029f36
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 17:01:54 +0000

    Definir estado inicial no construtor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "id",
+      login: "mojombo",
+      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo"
+    };
+  }
+
   render() {
-    return <div>UserItem</div>;
+    return <div className="card text-center"></div>;
   }
 }
 
