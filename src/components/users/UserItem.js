9e270dca7c612b56666add47be09d59fdeaddbc2
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 17:13:04 +0000

    Imagem com login e botão com url

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -21,6 +21,13 @@ class UserItem extends Component {
           className="round-img"
           style={{ width: "60px" }}
         />
+        <h3>{this.state.login}</h3>
+
+        <div>
+          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
       </div>
     );
   }
