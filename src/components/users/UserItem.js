c54e221f402908c7a908ecf51301c47ad20e0078
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 23:18:07 +0000

    Convert to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserItem extends Component {
-  render() {
-    //destructuring - pull the values out from the state
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = props => {
+  //destructuring - pull the values out from the state
+  const { login, avatar_url, html_url } = props.user;
 
-    return (
-      //irá mostrar uma imagem de acordo com o que está definido no estado
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
+  return (
+    //irá mostrar uma imagem de acordo com o que está definido no estado
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
