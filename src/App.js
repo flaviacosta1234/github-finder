5458f7e9530c209b07af2a7d416efa6bd1433eea
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 16:29:07 +0000

    Passar o icone como prop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar title="Github Finder" />
+        <Navbar title="Github Finder" icon="fab fa-github" />
       </div>
     );
   }
