ad17dd0edab9eab3136ba9d0fee3196d6b3431f0
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-09 17:06:36 +0000

    Imagem do avatar que estava guardada no estado do componente

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -12,7 +12,17 @@ class UserItem extends Component {
   }
 
   render() {
-    return <div className="card text-center"></div>;
+    return (
+      //irá mostrar uma imagem de acordo com o que está definido no estado
+      <div className="card text-center">
+        <img
+          src={this.state.avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "60px" }}
+        />
+      </div>
+    );
   }
 }
 
