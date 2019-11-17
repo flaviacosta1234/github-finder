cdadd34927c02daac9f8402215e3b3c451a61ca9
Flavia Costa <flavia.costa@talkdesk.com>
2019-11-17 11:07:57 +0000

    visit github profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -63,6 +63,9 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github profile
+            </a>
           </div>
         </div>
       </Fragment>
